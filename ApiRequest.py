d2c2bd2559208bf42bbeabf136615b1a266a12e6
hypnoticpenguin <chankevin234@gmail.com>
2020-12-14 20:51:58 -0500

    added ApiRequest.py file

---
+++ ApiRequest.py
@@ -0,0 +1,11 @@
+# file to call apis
+import requests
+
+class Races:
+    response = requests.get()
+    pass
+    
+
+
+if __name__ == '__main__':
+    print("This only executes when %s is executed rather than imported" % __file__)
