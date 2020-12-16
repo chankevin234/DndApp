39537e9580231829f710fff59f4a6c4625cd26d5
Danyal Mahmood <dmahmood@mail.uoguelph.ca>
2020-12-15 20:32:31 -0500

    add csv parser example

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,20 @@
 
 int main() {
     std::cout << "Hello World!" << "\n";
+
+    //show kc how to parse into easy to use classes
+    // while (getline(file, ID, ',')) {
+    //     cout << "ID: " << ID << " " ; 
+
+    //     getline(file, nome, ',') ;
+    //     cout << "User: " << nome << " " ;
+
+    //     getline(file, idade, ',') ;
+    //     cout << "Idade: " << idade << " "  ; 
+
+    //     getline(file, genero);
+    //     cout << "Sexo: " <<  genero<< " "  ;
+    // }
+
     return 0;
 }
