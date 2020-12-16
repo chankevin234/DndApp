e93ac15a268f34fa4ca2cb076c87df82fdcb3044
hypnoticpenguin <chankevin234@gmail.com>
2020-12-15 20:57:46 -0500

    added files for races info

--- ApiRequest.py
+++ ApiRequest.py
@@ -8,7 +8,13 @@ def getRacesResponse(): # get Races Request
     responseJSON = response.json()
     return(responseJSON)
 
-def convert_jsonObject_to_CSV():
+
+def getWeaponsResponse(): # get Weapons Request
+    response = requests.get("https://api.open5e.com/weapons/")
+    responseJSON = response.json()
+    return(responseJSON)
+
+def convert_jsonObject_to_CSV(): # get jsobObject and converts to dataframe
     jsonObject = getRacesResponse() # put json object into var              
     df = pd.DataFrame.from_dict(jsonObject) # turn var into dataframe
     
@@ -23,10 +29,13 @@ def convert_jsonObject_to_CSV():
         dfValues = pd.Series(list(df['results'][i].values()), index=output.columns) # this is all the data for each first instances within the 'results' key
         output = output.append(dfValues, ignore_index=True) # empty dataframe gets instances appended to it       
     print(output)
-    return(output)
+    cols = [0,2]    #selects specifically desired columns
+    dfEdit = output[output.columns[cols]]   #contains these columns
+    #print(dfEdit)
+    return(dfEdit)
 
 def main(): # main method
-    convert_jsonObject_to_CSV().to_csv('racesInfo.csv') # creates csv file  
+    convert_jsonObject_to_CSV().to_csv('csvFiles/racesInfo.csv', index=False) # creates raceCsv file  
     
 if __name__ == '__main__':
     print("This only executes when %s is executed rather than imported" % __file__)
